7e0fa092d57a8a91a5bf1c66137254c207224927
Aron-Kaniyarakath-Joseph <ajoseph2148@sdsu.edu>
2025-02-22 21:44:45 -0800

    add at beginning, end, and specific index

--- SpaceRoute.cpp
+++ SpaceRoute.cpp
@@ -2,59 +2,116 @@
 #include <string>
 using namespace std;
 
-template <typename T>
-class Node {
-public:
-    T& data;
-    Node* next;
-    Node* prev;
 
-    Node(T& d) : data(d), next(nullptr), prev(nullptr) {}
-    void print() { cout << data << " "; }
+template <typename T> class Node {
+    public:
+        T& data;
+        Node* next;
+        Node* prev;
+
+        Node(T& d) : data(d), next(nullptr), prev(nullptr) {}
+        void print() { cout << data << " "; }
 };
 
 class CelestialNode {
-private:
-    string name;
-    double distance_from_earth;
-    string mission_log;
+    private:
+        string name;
+        double distance_from_earth;
+        string mission_log;
 
-public:
-    CelestialNode(const string& n, double d, const string& log);
-    const string& getName() const;
-    double getDistance() const;
-    const string& getMissionLog() const;
-    void print() const { cout << name << " (" << distance_from_earth << " AU) - " << mission_log << endl; }
+    public:
+        CelestialNode(const string& n, double d, const string& log);
+        const string& getName() const;
+        double getDistance() const;
+        const string& getMissionLog() const;
+        void print() const { cout << name << " (" << distance_from_earth << " AU) - " << mission_log << endl; }
 };
 
-template <typename T>
-class SpaceRoute {
-private:
-    Node<T>* head;
-    Node<T>* tail;
 
-public:
-    SpaceRoute();  // Constructor
-    ~SpaceRoute(); // Destructor
+template <typename T> class SpaceRoute {
+    private:
+        Node<T>* head;
+        Node<T>* tail;
+        int length;
 
-    void addWaypointAtBeginning(T& data);
-    void addWaypointAtEnd(T& data);
-    void addWaypointAtIndex(int index, T& data);
-    void removeWaypointAtBeginning();
-    void removeWaypointAtEnd();
-    void removeWaypointAtIndex(int index);
-    void traverseForward();
-    void traverseBackward();
-    Node<T>* getWaypoint(int index);
-    void setWaypoint(int index, T& data);
-    void print(){
+    public:
+        SpaceRoute() {
+            head = nullptr;
+            tail = nullptr;
+            length = 0;
+        };  // Constructor
+        // ~SpaceRoute() {
+        //
+        // }; // Destructor
 
-            Node<T>* current = head;
-            while (current) {
-                current->print();
-                current = current->next;
+        void addWaypointAtBeginning(T& data) {
+            Node<T>* newNode = new Node(data);
+            //if the route is currently empty
+            if (head == nullptr) {
+                head = newNode;
+                tail = newNode;
             }
-            cout << endl;
+            //need to update head
+            else {
+                head->prev = newNode;
+                newNode->next = head;
+                head = newNode;
+            }
+            length++;
+        }
+        void addWaypointAtEnd(T& data){
+            Node<T>* newNode = new Node(data);
+            if (head == nullptr) {
+                head = newNode;
+                tail = newNode;
+            }
+            else {
+                newNode->prev = tail;
+                tail->next = newNode;
+                tail = newNode;
+            }
+            length++;
+        };
+    void addWaypointAtIndex(int index, T& data){
+        if (index < 0 || index > length + 1) {
+            cout <<"Invalid Index"<< endl;
+        }
+        //we can just call methods already defined in the case that we want to add at the beginning or the end
+        else if (index == 0) {
+            addWaypointAtBeginning(data);
+        }
+        else if (index == length) {
+            addWaypointAtEnd(data);
+        }
+        else {
+            Node<T>* newNode = new Node<T>(data);
+            Node<T>* curr = head;
+            for (int i = 0; i < index-1; i++) {
+                curr = curr->next;
+            }
+            newNode->next = curr->next;
+            curr->next->prev = newNode;
+            curr->next = newNode;
+            newNode->prev = curr;
+            length++;
+        }
+    };
+        void removeWaypointAtBeginning();
+        void removeWaypointAtEnd();
+        void removeWaypointAtIndex(int index);
+        void traverseForward();
+        void traverseBackward();
+        Node<T>* getWaypoint(int index);
+        void setWaypoint(int index, T& data);
+        void print(){
+
+                Node<T>* current = head;
+            cout<<"Printing..."<<endl;
+                while (current) {
+                    current->print();
+                    current = current->next;
+                }
+                cout << endl;
         }
 
 };
--- main.cpp
+++ main.cpp
@@ -10,27 +10,43 @@ int main() {
     string saturn = "Saturn";
     string earth = "Earth";
     string venus = "Venus";
+    string mercury = "Mercury";
 
-    SpaceRoute<string> voyagerRoute;
 
-    voyagerRoute.addWaypointAtEnd(mars);
-    voyagerRoute.addWaypointAtEnd(jupiter);
-    voyagerRoute.addWaypointAtEnd(saturn);
-    voyagerRoute.addWaypointAtBeginning(earth);
-    voyagerRoute.addWaypointAtIndex(2, venus);
-
-    cout << "Voyager Route (Forward):\n";
-    voyagerRoute.traverseForward();
-
-    cout << "\nVoyager Route (Backward):\n";
-    voyagerRoute.traverseBackward();
-
-    cout << "\nPrinting Route: \n";
+    SpaceRoute<string> voyagerRoute = SpaceRoute<string>();
+    //voyagerRoute.print();
+    voyagerRoute.addWaypointAtBeginning(mars);
     voyagerRoute.print();
-
-    voyagerRoute.removeWaypointAtIndex(2);
-    cout << "\nAfter Removing Venus: \n";
+    voyagerRoute.addWaypointAtBeginning(jupiter);
     voyagerRoute.print();
+    voyagerRoute.addWaypointAtBeginning(venus);
+    voyagerRoute.print();
+    voyagerRoute.addWaypointAtEnd(earth);
+    voyagerRoute.print();
+    voyagerRoute.addWaypointAtIndex(1,mercury);
+    voyagerRoute.print();
+    voyagerRoute.addWaypointAtIndex(3,mercury);
+    voyagerRoute.print();
+    cout << "Hello" <<endl;
+
+    // voyagerRoute.addWaypointAtEnd(mars);
+    // voyagerRoute.addWaypointAtEnd(jupiter);
+    // voyagerRoute.addWaypointAtEnd(saturn);
+    // voyagerRoute.addWaypointAtBeginning(earth);
+    // voyagerRoute.addWaypointAtIndex(2, venus);
+    //
+    // cout << "Voyager Route (Forward):\n";
+    // voyagerRoute.traverseForward();
+    //
+    // cout << "\nVoyager Route (Backward):\n";
+    // voyagerRoute.traverseBackward();
+    //
+    // cout << "\nPrinting Route: \n";
+    // voyagerRoute.print();
+    //
+    // voyagerRoute.removeWaypointAtIndex(2);
+    // cout << "\nAfter Removing Venus: \n";
+    // voyagerRoute.print();
 
     return 0;
 }
